cc01092787d7af5a7238139ccca7bdc4206c2f7f
David Martinez Urrea <david.martinez.dev@gmail.com>
2025-03-10 21:14:31 +0100

    Try to fix workflow, add content to Spring Boot Test

--- src/test/java/es/damarur/rock/paper/scissors/RockPaperScissorsApplicationTests.java
+++ src/test/java/es/damarur/rock/paper/scissors/RockPaperScissorsApplicationTests.java
@@ -1,13 +1,27 @@
 package es.damarur.rock.paper.scissors;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-@SpringBootTest
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@SpringBootTest(
+	classes = {RockPaperScissorsApplication.class},
+	webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+)
 class RockPaperScissorsApplicationTests {
+
+	@Autowired
+	private TestRestTemplate restTemplate;
 
 	@Test
 	void contextLoads() {
+		ResponseEntity<Void> health = restTemplate.getForEntity("/actuator/health", Void.class);
+		assertThat(health.getStatusCode()).isEqualTo(HttpStatus.OK);
 	}
 
 }
