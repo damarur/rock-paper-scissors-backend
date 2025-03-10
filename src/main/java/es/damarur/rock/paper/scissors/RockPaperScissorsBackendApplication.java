3f9548a66bcd7ecab9250fed920d02cedca2fe75
David Martinez Urrea <david.martinez.dev@gmail.com>
2025-03-10 14:56:20 +0100

    initial commit

---
+++ src/main/java/es/damarur/rock/paper/scissors/RockPaperScissorsBackendApplication.java
@@ -0,0 +1,13 @@
+package es.damarur.rock.paper.scissors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RockPaperScissorsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RockPaperScissorsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/damarur/rock/paper/scissors/RockPaperScissorsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package es.damarur.rock.paper.scissors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RockPaperScissorsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
