5c44f11b971b7c7638aaa6c824c464abe1b813ce
David Martinez Urrea <david.martinez.dev@gmail.com>
2025-03-14 15:19:48 +0100

    Add basic WebMvc configuration to allow cors for local development

---
+++ src/main/java/es/damarur/rock/paper/scissors/config/CustomWebMvcConfigurer.java
@@ -0,0 +1,27 @@
+package es.damarur.rock.paper.scissors.config;
+
+import lombok.NonNull;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CustomWebMvcConfigurer {
+
+	@Bean
+	public WebMvcConfigurer webMvcConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(@NonNull CorsRegistry registry) {
+				registry
+					.addMapping("/**")
+					.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+					.allowedOrigins("https://localhost", "http://localhost:4200")
+					.allowCredentials(false)
+					.exposedHeaders("Location");
+			}
+		};
+	}
+
+}
